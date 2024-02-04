703434f5e48ba894bf8cadf9c739d6a4adf14bfe
Francis Wekesa <135667995+Omusakhulu1@users.noreply.github.com>
2024-02-04 07:07:43 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+#This programs accespts user input like name, age, location and prints them out. 
+name = input('\nNigger, enter your name: ')
+age = int(input('And your age ooh? Please enter: '))
+location = input('Where do you stay? ')
+print('\nHello', name, 'Im not a witch but I know you are', age, 'years old and from', location,'.\n\n')
